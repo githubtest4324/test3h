e72e49b5c88c16927f22e2bfe05d41b872973509
fafafa <test2@github.github>
2014-07-25 12:59:22 +0300

    i18n

--- src/main/webapp/scripts/constants.js
+++ src/main/webapp/scripts/constants.js
@@ -12,13 +12,6 @@ test3hipsterApp.constant('USER_ROLES', {
 // It will be used by the library "http://www.famfamfam.com/lab/icons/flags/"
 // to display the flag
 test3hipsterApp.constant('FLAGS', {
-        da: 'dk',
         en: 'gb',
-        es: 'es',
-        fr: 'fr',
-        kr: 'kr',
-        de: 'de',
-        pl: 'pl',
-        ru: 'ru',
-        tr: 'tr'
+        ro: 'ro',
     });

4e19e3b786854df2d439220445493844e529281b
fafafa <test2@github.github>
2014-07-28 15:59:05 +0300

    bap

--- bap/view.js
+++ bap/view.js
@@ -1,34 +1,52 @@
 view = {
 		'requestMainFilter': {
 			type: 'form',
-			model: {
-				'creationDate': {
-					type: 'filterDateRange'
-				},
-				'customers': {
-					type: 'filterEntity',
-					action: {
-						open: {view: 'customerFilter'},
-						output: {
+			'creationDate': 'filterDateRange',
+			'customers': {
+				type: 'filterEntity',
+				readOnly: true,
+				action: {
+					'openCustomerFilter': {
+						type: 'openModal',
+						view: 'customerFilter',
+						inputBinding: {
+							'multiSelect': false
+						},
+						outputBinding: {
 							'customers.ids': 'ids',
 							'customers.names': 'names'
 						}
-						
-					}  
-				}
+
+					}
+
+				}  
 			}
 		},
 		'customerFilter':{
 			type: 'form',
-			{
+			input: {
+				'multiSelect': true
+			},
+			'criteria':{
 				type: 'form',
-				
+				'name': 'string'
+			},
+			action:{
+				"search":{
+					type: 'refreshGrid',
+					gridId: 'customerResult'
+				}
+			},
+			'customerResult':{
+				type:'grid',
+				columna
 			}
+
 		},
 		'requestMain':{
 			model: 'Request',
 		}
 
-		
-		
+
+
 };

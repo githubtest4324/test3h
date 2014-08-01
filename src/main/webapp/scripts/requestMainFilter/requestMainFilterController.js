1f59daa832909862704c828b286df1ee17123389
ffasfasf <gfsdfa@aaa.com>
2014-08-01 11:23:19 +0300

    request

--- src/main/webapp/scripts/requestMainFilter/requestMainFilterController.js
+++ src/main/webapp/scripts/requestMainFilter/requestMainFilterController.js
@@ -3,10 +3,26 @@
 test3hipsterApp.controller('requestMainFilterController', ['$scope', 'RequestMainFilter',
     function ($scope, RequestMainFilter) {
         $scope.criteria = {
-            customers: null,
+            customer: {
+                ids: null,
+                names: null,
+                openCustomerFilter: {
+                    viewId: 'customerFilter',
+                    viewParameters: {
+                        multiSelect: false //Aici am ramas: cum transfer multiselect la view.
+                    },
+                    viewOutput: null,
+                    perform: function () {
+                        $('#' + $scope.criteria.customer.openCustomerFilter.viewId).modal();
+                    }
+                }
+            },
             code: null,
             creationDateStart: null,
-            creationDateEnd: null
+            creationDateEnd: null,
+            customerFilter: {
+
+            }
         };
     }]);
 

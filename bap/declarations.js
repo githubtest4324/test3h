7065e66368b0ce4d29703521ad4bf9f4c6c39a84
ffasfasf <gfsdfa@aaa.com>
2014-08-03 10:33:24 +0300

    bap

--- bap/declarations.js
+++ bap/declarations.js
@@ -1,99 +1,191 @@
 var x1 = {
-		x2: "aaa",
-		x3: "bbb",
-		x4: 'ccc'
+	x2 : "aaa",
+	x3 : "bbb",
+	x4 : 'ccc'
 };
 
 var y1 = {
-		x2: "aaa",
-		x3: "bbb",
-		x4: 'ccc'
+	x2 : "aaa",
+	x3 : "bbb",
+	x4 : 'ccc'
 };
 
-//Field types
-var any = {fieldType: 'anyType'}; // Defalut type
-var id = {fieldType: 'idType'};
-var str = {fieldType: 'stringType'};
-var date = {fieldType: 'dateType'}; // Contains both date and time
-var bool = {fieldType: 'booleanType'};
-var number = {fieldType: 'numberType'};
-var func = {fieldType: 'functionType'};
+// Field types
+// Default type
+var any = {
+	fieldType : 'anyType'
+};
+// Reference to any definition
+var ref = {
+	fieldType : 'referenceType',
+};
+var id = {
+	fieldType : 'idType'
+};
+var str = {
+	fieldType : 'stringType'
+};
+// Contains both date and time
+var date = {
+	fieldType : 'dateType'
+};
+var bool = {
+	fieldType : 'booleanType'
+};
+var num = {
+	fieldType : 'numberType'
+};
+var func = {
+	fieldType : 'functionType'
+};
 
+// Form display types
+var none = {
+	type : str,
+	displayType : 'label'
+};
+var label = {
+	type : str,
+	displayType : 'label'
+};
+var text = {
+	type : str,
+	displayType : 'text'
+};
+var textArea = {
+	type : str,
+	displayType : 'textArea'
+};
+var checkBox = {
+	type : bool,
+	displayType : 'checkBox'
+};
+var datePicker = {
+	type : date,
+	displayType : 'datePicker'
+};
+var timePicker = {
+	type : date,
+	displayType : 'timePicker'
+};
+var dateTimePicker = {
+	type : date,
+	displayType : 'dateTimePicker'
+};
 
-//Simple views
-var label = {type: str, displayType: 'label'};
-var text = {type: str, displayType: 'text'};
-var textArea = {type: str, displayType: 'textArea'};
-var checkBox = {type: bool, displayType: 'checkBox'};
-var datePicker = {type: date, displayType: 'datePicker'};
-var timePicker = {type: date, displayType: 'timePicker'};
-var dateTimePicker = {type: date, displayType: 'dateTimePicker'};
-
-//Filter views
+// Filter views
 var filterEntity = {
-		display: 'filterEntityDisplay',
-		'ids': str,
-		'names': str
+	display : 'filterEntityDisplay',
+	'ids' : str,
+	'names' : str
 };
 var filterDateRange = {
-		'startDate': {
-			type: datePicker
-		},
-		'endDate': datePicker
+	'startDate' : {
+		type : datePicker
+	},
+	'endDate' : datePicker
 };
 
+// Container views
+var page = {
+	viewType : 'pageView',
 
-//Container views
-var page = {viewType: 'pageView'};
-var form = {viewType: 'formView'};
+	// Reference to model used as data source
+	model : ref,
+
+	// Defines list of fields to be shown from model. If model
+	// is not defined, this is considered the model.
+	fields : any,
+};
+var form = {
+	viewType : 'formView',
+
+	// Reference to model used as data source
+	model : ref,
+
+	// Defines list of fields to be shown from model. If model
+	// is not defined, this is considered the model.
+	fields : any,
+};
 var grid = {
-		viewType: 'gridView',
-		
-		// Grid options
-		options: {
-			selectable: {type: bool, defaultValue: true},
-			multiSelection: {type: bool, defaultValue: true},
-            columnResize: {type: bool, defaultValue: true},
-            columnReorder: {type: bool, defaultValue: true},
-            sortable: {type: bool, defaultValue: true},
-            headerRowHeight: {type: num, defaultValue: 35},
-            rowHeight: {type: num, defaultValue: 35},
-		},
+	viewType : 'gridView',
 
-		// idsOut getSelectedIds() - Returns comma separated list of selected ids
-		selectedIds: {
-			type: func,
-			functionType: 'gridSelectedIds',
-			'idsOut': str
+	// Reference to model used as data source
+	model : ref,
+
+	// Defines list of fields to be shown from model. If model
+	// is not defined, this is considered the model.
+	fields : any,
+
+	// Grid options
+	options : {
+		selectable : {
+			type : bool,
+			defaultValue : true
 		},
-		
-		// valuesOut getSelectedValues(fieldName) - Returns comma separated list of selected values for 'fieldName'
-		selectedValues: {
-			type: func,
-			functionType: 'gridSelectedValues',
-			'fieldName': str,
-			'valuesOut': str,
-		}
+		multiSelection : {
+			type : bool,
+			defaultValue : true
+		},
+		columnResize : {
+			type : bool,
+			defaultValue : true
+		},
+		columnReorder : {
+			type : bool,
+			defaultValue : true
+		},
+		sortable : {
+			type : bool,
+			defaultValue : true
+		},
+		headerRowHeight : {
+			type : num,
+			defaultValue : 35
+		},
+		rowHeight : {
+			type : num,
+			defaultValue : 35
+		},
+	},
+
+	// idsOut getSelectedIds() - Returns comma separated list of selected ids
+	selectedIds : {
+		type : func,
+		functionType : 'gridSelectedIds',
+		'idsOut' : str
+	},
+
+	// valuesOut getSelectedValues(fieldName) - Returns comma separated list of
+	// selected values for 'fieldName'
+	selectedValues : {
+		type : func,
+		functionType : 'gridSelectedValues',
+		'fieldName' : str,
+		'valuesOut' : str,
+	}
 };
 
-//Actions
+// Actions
 var openModal = {
-		actionType: 'openModalAction',
-		'view': {type: form, mandatory: true},
-		'viewParameters': any,
-		'viewOutput': any
+	actionType : 'openModalAction',
+	'view' : {
+		type : form,
+		mandatory : true
+	},
+	'viewParameters' : any,
+	'viewOutput' : any
 };
 var refreshGrid = {
-		actionType: 'refreshGridAction',
-		'gridView': {type: form, mandatory: true}
+	actionType : 'refreshGridAction',
+	'gridView' : {
+		type : form,
+		mandatory : true
+	}
 };
 var back = {
-		actionType: 'backAction'
+	actionType : 'backAction'
 };
 var customAction = {
-		actionType: 'customAction'
+	actionType : 'customAction'
 };
-
-
-
-
--- bap/view.js
+++ bap/view.js
@@ -3,113 +3,120 @@
 var fs = require('fs');
 var vm = require('vm');
 function include(path) {
-    var code = fs.readFileSync(path, 'utf-8');
-    vm.runInThisContext(code, path);
+	var code = fs.readFileSync(path, 'utf-8');
+	vm.runInThisContext(code, path);
 }
 
 include('declarations.js');
 
-
 var view = {
-    'requestMainFilter': {
-        type: page,
-        // Criteria
-        'criteria': {
-            type: form,
-            'creationDate': 'filterDateRange',
-            'code': text,
-            'customer': {
-                type: filterEntity,
-                readOnly: true,
-                'openCustomerFilter': {
-                    type: openModal,
-                    before: {
-                        bind: {
-                            '$view': '$customerFilter',
-                            '$customerFilter.grid.options.multiSelect': false
-                        }
-                    },
-                    after: {
-                        bind: {
-                            '$customer.ids': '$customerFilter.idsOut',
-                            '$customer.names': '$customerFilter.namesOut'
-                        }
-                    }
-                }
-            }
-        },
-        // Search action
-        'search':{
-            type: refreshGrid,
-            before: {
-                bind: {
-                    '$gridView': '$requestGrid'
-                }
-            }
-        },
-        // Grid
-        'requestGrid':{
-            type: grid,
-            'id': id,
-            'code': str,
-            'creationDate': date,
-            'expectedDeliveryDate': date,
-            'customer': str
-        }
+	'requestMainFilter' : {
+		type : page,
+		fields : {
+			// Criteria
+			'criteria' : {
+				type : form,
+				fields:{
+					'creationDate' : filterDateRange,
+					'code' : text,
+					'customer' : {
+						type : filterEntity,
+						readOnly : true,
+						'openCustomerFilter' : {
+							type : openModal,
+							before : {
+								bind : {
+									'$view' : '$customerFilter',
+									'$customerFilter.grid.options.multiSelect' : false
+								}
+							},
+							after : {
+								bind : {
+									'$customer.ids' : '$customerFilter.idsOut',
+									'$customer.names' : '$customerFilter.namesOut'
+								}
+							}
+						}
+					}
+				}
+			},
+			// Search action
+			'search' : {
+				type : refreshGrid,
+				before : {
+					bind : {
+						'$gridView' : '$requestGrid'
+					}
+				}
+			},
+			// Grid
+			'requestGrid' : {
+				type : grid,
+				fields : {
+					'id' : none,
+					'code' : text,
+					'creationDate' : dateTimePicker,
+					'expectedDeliveryDate' : datePicker,
+					'customer' : text
+				}
+			}
+		}
+	},
+	'customerFilter' : {
+		type : form,
+		// Output parameters
+		'idsOut' : str,
+		'namesOut' : str,
+		// Criteria
+		'criteria' : {
+			type : form,
+			'name' : str
+		},
+		// Search Action
+		'search' : {
+			type : refreshGrid,
+			before : {
+				bind : {
+					'$gridView' : '$grid'
+				}
+			}
+		},
+		// Grid
+		'grid' : {
+			type : grid,
+			fields : {
+				'id' : none,
+				'name' : text,
+				'code' : text
+			}
+		},
+		// Ok/Cancel Actions
+		'ok' : {
+			type : customAction,
+			before : {
+				bind : {
+					'$idsOut' : 'grid.selectedIds()',
+					'$customerGrid.selectedValues.fieldName' : 'name',
+					'$namesOut' : '$grid.selectedValues()'
+				},
+			},
+			after : {
+				'goBack' : back
+			}
+		},
+		'cancel' : back
 
-    },
-    'customerFilter': {
-        type: form,
-        // Output parameters
-        'idsOut': str,
-        'namesOut': str,
-        // Criteria
-        'criteria': {
-            type: form,
-            'name': str
-        },
-        // Search Action
-        'search': {
-            type: refreshGrid,
-            before: {
-                bind: {'$gridView': '$grid'}
-            }
-        },
-        // Grid
-        'grid': {
-            type: grid,
-            'name': str,
-            'code': str
-        },
-        // Ok/Cancel Actions
-        'ok': {
-            type: customAction,
-            before: {
-                bind: {
-                    '$idsOut': 'grid.selectedIds()',
-                    '$customerGrid.selectedValues.fieldName': 'name',
-                    '$namesOut': '$grid.selectedValues()'
-                },
-                after: {
-                    'goBack': back
-                }
-            }
-        },
-        'cancel': back
+	},
+	'testModule' : {
+		type : form,
+		't1' : str,
+		'criteria' : {
+			type : form,
+			'creationDate' : filterDateRange
+		}
 
-    },
-    'testModule': {
-        type: form,
-        't1': str,
-        'criteria': {
-            type: form,
-            'creationDate': filterDateRange
-        }
-
-    }
-
+	}
 
 };
 
 console.log(JSON.stringify(view, null, 2));
-
